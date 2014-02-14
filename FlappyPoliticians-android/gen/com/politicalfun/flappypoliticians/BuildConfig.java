2757cfa8c9e93c5d77d383d50bd7902797a71f65
bharathy89 <bharath.yarlagadda89@gmail.com>
2014-02-14 14:17:09 -0500

    day2&3

---
+++ FlappyPoliticians-android/gen/com/politicalfun/flappypoliticians/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.politicalfun.flappypoliticians;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- FlappyPoliticians-desktop/src/com/politicalfun/flappypoliticians/Main.java
+++ FlappyPoliticians-desktop/src/com/politicalfun/flappypoliticians/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "FlappyPoliticians";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 272;
+		cfg.height = 408;
 		
 		new LwjglApplication(new FPGame(), cfg);
 	}
